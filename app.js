4275c54fe2fc9c5f831f36694d92b17117451976
Alaina Barnes <acbarnes1@my.waketech.edu>
2026-05-21 14:16:00 -0400

    uploading files

---
+++ app.js
@@ -0,0 +1,16 @@
+import { users } from "./data.js";
+
+const findUserById = (id) => {
+  const user = users.find((u) => u.id === id);
+
+  if (user == null) return "Error: User not found";
+
+  const { name, email } = user;
+  return "ID: " + id + " | Name: " + name + " | Email: " + email;
+};
+
+//test cases for fuctions
+//id that exists
+console.log(findUserById(2));
+//id that doesn't
+console.log(findUserById(99));
---
+++ data.js
@@ -0,0 +1,6 @@
+export const users = [
+  { id: 1, name: "Alaina Barnes", email: "acbarnes1@my.waketech.edu" },
+  { id: 2, name: "Frida Kahlo", email: "fridawashere@email.com" },
+  { id: 3, name: "Vincent Van Gogh", email: "staryyy_night@email.com" },
+  { id: 4, name: "Pablo Picasso", email: "cubism-pablo@email.com" },
+];
